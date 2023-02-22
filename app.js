713f24b7c42b1de44284c1707c65496f54ac7551
Alfaya <fcalfaya@gmail.com>
2023-02-22 11:21:52 -0300

    test

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const port = 4000
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log(req)
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(port, () => {
+    console.log([`${process.env.npm_package_name} listening on port ${port}`])
+})
+
+process.on("uncaughtException",(error,origin)=>{
+    console.log([`\n${origin} -  ${error}`])
+})
+
+process.on("unhandledRejection",(error)=>{
+    console.log([`\n rejeitado -  ${error}`])
+})
